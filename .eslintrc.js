abe23d1ac998803a5fce22d03fc31f4e7c205d72
liguodi <guodi_li@fpi-inc.com>
2023-11-24 17:05:00 +0800

    调整校验配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,25 @@
 module.exports = {
-  root: true,
+    root: true,
 
-  env: {
-    node: true
-  },
+    env: {
+        node: true
+    },
 
-  extends: [
-    'plugin:vue/essential',
-    'plugin:vue/strongly-recommended',
-    '@vue/standard'
-  ],
+    extends: [
+        'plugin:vue/essential',
+        'plugin:vue/strongly-recommended',
+        '@vue/standard'
+    ],
 
-  parserOptions: {
-    parser: '@babel/eslint-parser'
-  },
+    parserOptions: {
+        parser: '@babel/eslint-parser'
+    },
 
-  rules: {
-    'no-console': 'off',
-    'no-debugger': 'off',
-    'vue/require-default-prop': 'off'
-  }
+    rules: {
+        'no-console': 'off',
+        'no-debugger': 'off',
+        'vue/require-default-prop': 'off',
+        'vue/html-indent': ['error', 4],
+        indent: ['error', 4]
+    }
 }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="home">
-    <img
-      alt="Vue logo"
-      src="../assets/logo.png"
-    >
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+    <div class="home">
+        <img
+            alt="Vue logo"
+            src="../assets/logo.png"
+        >
+        <HelloWorld msg="Welcome to Your Vue.js App" />
+    </div>
 </template>
 
 <script>
@@ -13,9 +13,9 @@
 import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+    name: 'HomeView',
+    components: {
+        HelloWorld
+    }
 }
 </script>
